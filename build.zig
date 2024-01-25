7268d67d0306c42fe6bbe9412060648558fbb6a6
Chris Marchesi <chrism@vancluevertech.com>
2024-01-25 10:02:22 -0800

    build: simplify "spec" target
    Moves update for the E2E tests to a build-time option (-Dupdate=true).

--- build.zig
+++ build.zig
@@ -18,28 +18,28 @@ pub fn build(b: *std.Build) void {
     test_step.dependOn(&test_run.step);
 
     // Spec tests are complex E2E tests that render to files for comparison.
-    // Use "zig build genspec" to generate the files used by this test. The
+    // Use "zig build spec -Dupdate=true" to generate the files used by this test. The
     // test code itself is found in "spec".
-    const spec_test = b.addTest(.{
-        .name = "spec",
-        .root_source_file = .{ .path = "spec/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+    const spec_update = b.option(
+        bool,
+        "update",
+        "Update spec (E2E) tests (needs to be run with the \"spec\" target",
+    );
+    const spec_test = spec: {
+        const opts = .{
+            .name = "spec",
+            .root_source_file = .{ .path = "spec/main.zig" },
+            .target = target,
+            .optimize = optimize,
+        };
+        if (spec_update orelse false)
+            break :spec b.addExecutable(opts)
+        else
+            break :spec b.addTest(opts);
+    };
+
     spec_test.root_module.addImport("z2d", z2d);
     const spec_test_run = b.addRunArtifact(spec_test);
     const spec_test_step = b.step("spec", "Run spec (E2E) tests");
     spec_test_step.dependOn(&spec_test_run.step);
-
-    // Generation of spec tests
-    const genspec = b.addExecutable(.{
-        .name = "genspec",
-        .root_source_file = .{ .path = "spec/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    genspec.root_module.addImport("z2d", z2d);
-    const genspec_run = b.addRunArtifact(genspec);
-    const genspec_step = b.step("genspec", "Generate spec tests");
-    genspec_step.dependOn(&genspec_run.step);
 }
