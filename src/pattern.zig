c291464790864e40772a868d8957eacfe449e597
Chris Marchesi <chrism@vancluevertech.com>
2024-01-26 11:28:20 -0800

    pattern: simplify OpaquePattern, make it a value vs reference
    After thinking about this more, I've decided to simplify OpaquePattern
    for now. We don't need the memory management part yet, and I'm not too
    sure when we will. Currently, the pattern exists to provide scaffolding
    to the concept of an interface that we will need eventually (e.g. when
    we allow surfaces to also be presented as patterns), but I'm not too
    sure when that will be, and the tagged union structure means that we
    don't need to have every implementation be 100% uniform (e.g. one can be
    a value, another can be a reference).

--- src/pattern.zig
+++ src/pattern.zig
@@ -1,4 +1,3 @@
-const mem = @import("std").mem;
 const testing = @import("std").testing;
 
 const pixelpkg = @import("pixel.zig");
@@ -10,67 +9,31 @@ pub const PatternType = enum {
 
 /// Represents an interface as a union of all patterns.
 pub const Pattern = union(PatternType) {
-    opaque_pattern: *OpaquePattern,
-
-    /// Releases the underlying pattern reference, and anything else as
-    /// applicable. The pattern is invalid for use afterwards.
-    pub fn deinit(self: Pattern) void {
-        return switch (self) {
-            .opaque_pattern => |s| s.alloc.destroy(s),
-        };
-    }
+    opaque_pattern: OpaquePattern,
 
     /// Gets the pixel data at the co-ordinates specified.
     pub fn getPixel(self: Pattern, x: u32, y: u32) !pixelpkg.Pixel {
+        _ = x;
+        _ = y;
         return switch (self) {
-            .opaque_pattern => |s| s.getPixel(x, y),
+            .opaque_pattern => |s| s.pixel,
         };
     }
 };
 
-/// Creates an opaque pattern as an interface.
-///
-/// The caller owns the memory, so make sure to call deinit on the pattern to
-/// release it.
-pub fn createOpaquePattern(
-    alloc: mem.Allocator,
-    pixel: pixelpkg.Pixel,
-) !Pattern {
-    const op = try alloc.create(OpaquePattern);
-    op.* = .{ .alloc = alloc, .pixel = pixel };
-    return op.asPatternInterface();
-}
-
 /// A simple opaque color pattern that writes the set color to every pixel.
 pub const OpaquePattern = struct {
-    /// The underlying allocator, Should not be used.
-    alloc: mem.Allocator,
-
     /// The underlying pixel for this pattern.
     pixel: pixelpkg.Pixel,
 
     /// Returns a Pattern interface for this surface.
-    pub fn asPatternInterface(self: *OpaquePattern) Pattern {
+    pub fn asPatternInterface(self: OpaquePattern) Pattern {
         return .{ .opaque_pattern = self };
-    }
-
-    /// Gets the pixel data at the co-ordinates specified.
-    ///
-    /// Note that OpaquePattern returns its underlying pixel for every
-    /// co-ordinate passed; the co-ordinates specified are only for interface
-    /// implementation.
-    pub fn getPixel(self: *OpaquePattern, x: u32, y: u32) !pixelpkg.Pixel {
-        // Note that we just discard x and y as the same pixel is returned for
-        // every location.
-        _ = x;
-        _ = y;
-        return self.pixel;
     }
 };
 
 test "OpaquePattern, as interface" {
     const px: pixelpkg.Pixel = .{ .rgb = .{ .r = 0xAA, .g = 0xBB, .c = 0xCC } };
-    const pt = try createOpaquePattern(testing.allocator, px);
-    defer pt.deinit();
+    const pt: Pattern = .{ .opaque_pattern = .{ .pixel = px } };
     try testing.expectEqual(px, pt.getPixel(1, 1));
 }
