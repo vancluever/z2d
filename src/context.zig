d73d0133e1cd21250bc0f8eb1dad2d69a08c1caf
Chris Marchesi <chrism@vancluevertech.com>
2024-01-26 13:47:57 -0800

    context: add setPattern
    This allows the pattern to be set on the context, with checks for pixel
    format compatibility.

--- src/context.zig
+++ src/context.zig
@@ -6,7 +6,7 @@ const pixelpkg = @import("pixel.zig");
 
 /// The draw context, which connects patterns to surfaces, holds other state
 /// data, and is used to dispatch drawing operations.
-const DrawContext = struct {
+pub const DrawContext = struct {
     pattern: patternpkg.Pattern,
     surface: surfacepkg.Surface,
 
@@ -22,9 +22,28 @@ const DrawContext = struct {
             .surface = surface,
         };
     }
+
+    /// Sets the pattern on the context to the supplied pattern.
+    ///
+    /// Returns an error if the pattern would be incompatible for the context's
+    /// underlying surface.
+    pub fn setPattern(self: *DrawContext, pattern: patternpkg.Pattern) !void {
+        // Do a simple check for now on the underlying pattern interface to
+        // check for compatibility, with the surface's pixel format. This will
+        // eventually change so that we can do transformations, etc.
+        switch (pattern) {
+            .opaque_pattern => |p| {
+                if (p.pixel != self.surface.format()) {
+                    return error.IncompatiblePatternForSurface;
+                }
+            },
+        }
+
+        self.pattern = pattern;
+    }
 };
 
-test "DrawContext" {
+test "DrawContext, basic" {
     const sfc = try surfacepkg.createSurface(.image_surface_rgba, testing.allocator, 1, 1);
     defer sfc.deinit();
 
@@ -32,4 +51,27 @@ test "DrawContext" {
     try ctx.surface.putPixel(0, 0, try ctx.pattern.getPixel(0, 0));
     const expected_px: pixelpkg.Pixel = .{ .rgba = .{ .r = 0x00, .g = 0x00, .b = 0x00, .a = 0xFF } };
     try testing.expectEqual(expected_px, try ctx.surface.getPixel(0, 0));
+}
+
+test "DrawContext, setPattern, OK" {
+    const sfc = try surfacepkg.createSurface(.image_surface_rgb, testing.allocator, 1, 1);
+    defer sfc.deinit();
+
+    var ctx = DrawContext.init(sfc);
+    const expected_px: pixelpkg.Pixel = .{ .rgb = .{ .r = 0xAA, .g = 0xBB, .b = 0xCC } };
+    try ctx.setPattern(.{ .opaque_pattern = .{ .pixel = expected_px } });
+    try ctx.surface.putPixel(0, 0, try ctx.pattern.getPixel(0, 0));
+    try testing.expectEqual(expected_px, try ctx.surface.getPixel(0, 0));
+}
+
+test "DrawContext, setPattern, invalid pixel format" {
+    const sfc = try surfacepkg.createSurface(.image_surface_rgba, testing.allocator, 1, 1);
+    defer sfc.deinit();
+
+    var ctx = DrawContext.init(sfc);
+    const px: pixelpkg.Pixel = .{ .rgb = .{ .r = 0xAA, .g = 0xBB, .b = 0xCC } };
+    try testing.expectError(
+        error.IncompatiblePatternForSurface,
+        ctx.setPattern(.{ .opaque_pattern = .{ .pixel = px } }),
+    );
 }
