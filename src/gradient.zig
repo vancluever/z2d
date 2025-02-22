31b0bef00cbc659fadb550a5acb945c18625149e
Chris Marchesi <chrism@vancluevertech.com>
2025-02-22 14:28:15 -0800

    gradient: add license statement to radial gradient algo comments
    Different parts of Pixman (actually, lots of Pixman :P) have
    terms/language that more resemble the Prior BSD License versus the MIT
    license as seen in the COPYING file. While the notice is a bit meaty I
    don't necessarily see the issue with adding the full statement to the
    comments breaking down the radial gradient algorithm, so I've done that
    now.

--- src/gradient.zig
+++ src/gradient.zig
@@ -376,6 +376,36 @@ pub const Radial = struct {
     // A < 0 <=> one of the two circles completely contains the other one
     //   <=> for every p, the radiuses associated with the two t solutions
     //       have opposite sign
+    //
+    // ---
+    //
+    // The following copyright notice applies to the derived code from
+    // pixman-radial-gradient.c:
+    //
+    // Copyright © 2000 Keith Packard, member of The XFree86 Project, Inc.
+    // Copyright © 2000 SuSE, Inc.
+    //             2005 Lars Knoll & Zack Rusin, Trolltech
+    // Copyright © 2007 Red Hat, Inc.
+    //
+    // Permission to use, copy, modify, distribute, and sell this software and
+    // its documentation for any purpose is hereby granted without fee,
+    // provided that the above copyright notice appear in all copies and that
+    // both that copyright notice and this permission notice appear in
+    // supporting documentation, and that the name of Keith Packard not be used
+    // in advertising or publicity pertaining to distribution of the software
+    // without specific, written prior permission.  Keith Packard makes no
+    // representations about the suitability of this software for any purpose.
+    // It is provided "as is" without express or implied warranty.
+    //
+    // THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS
+    // SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+    // FITNESS, IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    // SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+    // RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+    // CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+    // CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+    //
+    // ---
 
     /// Calculates the offset based on the distance from the two centers of the
     /// supplied point in comparison to their radii. This offset can be used to
